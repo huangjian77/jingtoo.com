57c20b3a2ea3f4e14a94d86a7d523d1c99bb5ff7
huangjian77 <huangj@jingtoo.com>
2013-04-02 12:19:11 +0800

    新增栏目表，文章表

--- Sql/table.sql
+++ Sql/table.sql
@@ -16,3 +16,20 @@ CREATE TABLE IF NOT EXISTS `tb_cms_category` (
   `created_at` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
   PRIMARY KEY (`id`)
 )
+-- 文章列表
+CREATE TABLE IF NOT EXISTS `tb_cms_category` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父类ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '分类名称',
+  `display_order` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '排序值',
+  `is_show` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否显示',
+  `url` varchar(255) NOT NULL DEFAULT '' COMMENT '访问该栏目的url',
+  `content_type` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '分类展示的内容的类型',
+  `link_url` varchar(255) NOT NULL DEFAULT '' COMMENT '链接地址',
+  `link_target` varchar(50) NOT NULL DEFAULT '' COMMENT '链接的目标',
+  `article_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '显示文章的id',
+  `keyword` varchar(255) NOT NULL DEFAULT '' COMMENT '分类的关键字',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '分类的描述',
+  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
