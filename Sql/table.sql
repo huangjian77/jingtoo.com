afddc8042516d2af2c3229fe9fc0b10ca28a3da6
huangjian77 <huangj@jingtoo.com>
2013-04-07 18:05:03 +0800

    创建时间 改成datetime类型

--- Sql/table.sql
+++ Sql/table.sql
@@ -3,8 +3,8 @@
 CREATE TABLE `jt_admin` (
   `login_name` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '管理员登陆名',
   `name` char(32) NOT NULL COMMENT '管理员登陆密码',
-  `password` char(32) DEFAULT 'md5',
-  `last_time` int(11) DEFAULT '0',
+  `password` char(32) COMMENT 'md5',
+  `last_time` datetime,
   `last_ip` char(15) DEFAULT '',
   PRIMARY KEY (`mid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='后台管理员表'
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `jt_cms_category` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `parent_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '父类ID',
   `name` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '分类名称',
-  `display_order` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT '排序值',
+  `display_order` VARCHAR(50) UNSIGNED NOT NULL DEFAULT '0' COMMENT '排序值',
   `is_show` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否显示',
   `url` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '访问该栏目的url',
   `content_type` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '分类展示的内容的类型 文章列表、指定文章、内部链接、外部连接',
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `jt_cms_category` (
   `article_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '显示文章的id',
   `keyword` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '分类的关键字',
   `description` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '分类的描述',
-  `created_at` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='栏目表';
 -- 文章列表
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `jt_cms_article` (
   `views` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '浏览次数',
   `display_order` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '排序值',
   `is_show` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否显示',
-  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `updated_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最后更新时间',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
+  `updated_at` datetime NOT NULL COMMENT '最后更新时间',
   `picture` varchar(255) NOT NULL DEFAULT '' COMMENT '缩略图地址',
   `content` text NOT NULL COMMENT '内容',
   PRIMARY KEY (`id`)
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `jt_site_friendlink` (
   `logo_pic` varchar(255) NOT NULL DEFAULT '' COMMENT 'logo上传图片地址',
   `description` varchar(255) NOT NULL DEFAULT '' COMMENT '网站描述',
   `owner_email` varchar(50) NOT NULL DEFAULT '' COMMENT '站长email',
-  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 -- 站点广告位表
@@ -66,7 +66,7 @@ CREATE TABLE `jt_site_ad` (
   `default_code` text NOT NULL COMMENT '默认代码',
   `start_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '开始时间',
   `end_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '结束时间',
-  `created_at` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='站点广告位表';
 
